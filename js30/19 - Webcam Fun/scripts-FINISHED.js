43ed1100b25812e5830802775860ae219adc0686
Wade <wsw0615@gmail.com>
2020-03-10 12:40:28 +0800

    19-feat: done

--- js30/19 - Webcam Fun/scripts-FINISHED.js
+++ js30/19 - Webcam Fun/scripts-FINISHED.js
@@ -32,16 +32,14 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
-    // mess with them
+
     // pixels = redEffect(pixels);
 
     pixels = rgbSplit(pixels);
     // ctx.globalAlpha = 0.8;
 
     // pixels = greenScreen(pixels);
-    // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
